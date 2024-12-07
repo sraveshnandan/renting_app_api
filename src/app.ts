faf5ed16e66cb296e5194a8c7b050454e942436e
sraveshnandan <kumarsravesh39@gmail.com>
2024-12-08 04:13:36 +0530

    cors issue fixed

--- src/app.ts
+++ src/app.ts
@@ -5,62 +5,52 @@ import { resolvers, typeDefs } from "./graphql";
 import { MongoDbUri, Port } from "./config";
 import UploadRoute from "./routes";
 import express from "express";
-import { sendEmail } from "./lib";
+import cors from "cors";
 
+// express server
+const app = express();
 
+app.use(cors());
 
-
-// express server 
-const app = express()
-
-// file upload middlewares 
-
-
-
-// middlewares 
+// middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/v1/upload", UploadRoute);
 
-app.use("/api/v1/upload", UploadRoute)
-
-// aplollo server 
+// aplollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-
-
-// graphql server 
+// graphql server
 const startGrapQlServer = async () => {
-  console.log("connecting to the database.")
+  console.log("connecting to the database.");
   mongoose
     .connect(MongoDbUri)
     .then(async (con) => {
       console.log(`📡 Databse is connected to : ${con.connection.host}`);
       console.log(`Starting GraphQl Server.`);
       await server.start();
-      app.use("/graphql", expressMiddleware(server, {
-        // options 
-        context: async ({ req }) => { return req.headers }
-      }))
+      app.use(
+        "/graphql",
+        expressMiddleware(server, {
+          // options
+          context: async ({ req }) => {
+            return req.headers;
+          },
+        }),
+      );
       app.listen(Port, () => {
-        console.log("graphql server started on http://localhost:5000/graphql")
-      })
-
-
+        console.log("graphql server started on http://localhost:5000/graphql");
+      });
     })
     .catch((error) =>
-      console.log(`Unable to connect to the database due to : ${error.message}`)
+      console.log(
+        `Unable to connect to the database due to : ${error.message}`,
+      ),
     );
 };
-
-
-
-
-
-
-
 
 startGrapQlServer();
